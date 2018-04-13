16a531fbc54263b9332afa05d94e97e8a4d8016b
Hnatekmar <m.hnatma@gmail.com>
2018-04-13 21:40:09 +0200

    Added derivations for activation functions.

--- Activations.py
+++ Activations.py
@@ -2,14 +2,18 @@ import numpy as np
 
 class Relu:
     def derivative(self, x):
-        return np.maximum(0, x)
+        result = self(x)
+        result[result > 0] = 1
+        return result
 
     def __call__(x):
         return np.maximum(0, x)
 
 class Sigmoid:
+    EPSILON = 1e-25
     def derivative(self, x):
-        return 0
+        sigmoid = self(x)
+        return (1.0 - sigmoid) * sigmoid
 
     def __call__(self, x):
-        return 1.0 / (1.0 + np.exp(-x))
+        return 1.0 / (1.0 + np.exp(-x + EPSILON))
